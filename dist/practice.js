cf7b1903401e7c189d096dce2bd46798e7e89081
DongHyun Kim <developerforus@gmail.com>
2022-05-02 20:42:38 +0900

    ts

--- dist/practice.js
+++ dist/practice.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log(123);
+class Person {
+    constructor(name, age, address) {
+        this.thisCheck = () => {
+            return this;
+        };
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+}
+const me = new Person("kim");
+console.log(me.thisCheck());
+class Foo {
+    constructor(me) {
+        this.me = me;
+    }
+    identify() {
+        return "I am " + this.me;
+    }
+}
+const foo = new Foo("kim");
+console.log(foo.identify());
