31762af77f41d488dc4c5ea013bd13a62e2ded59
DongHyun Kim <developerforus@gmail.com>
2022-05-24 23:57:17 +0900

    let const var

---
+++ practice2/variable.js
@@ -0,0 +1,70 @@
+// // var name = "Evan";
+// // var name = "Evan2";
+// // console.log(name); // Evan2
+
+// console.log(name); // undefined
+// var name = "kim";
+
+// (function () {
+//   var local = 1;
+// })();
+// // console.log(local);
+// // Uncaught ReferenceError: local is not defined
+
+// for (var i = 0; i < 10; i++) {}
+// console.log(i); // 10
+
+// var globalVariable = "global!";
+
+// if (globalVariable === "global!") {
+//   globlVariable = "global?"; // 오타 냄
+// }
+
+// console.log(globalVariable); // global!
+// console.log(globlVariable); // global?
+
+// const value = "kim";
+// value = "lee"; //TypeError: Assignment to constant variable.
+
+// var name = "kim";
+// var name = "lee"; // 아무 일도 일어나지 않았다...
+
+// let name = "kim";
+// let name = "lee";
+// // Uncaught SyntaxError: Identifier 'name' has already been declared
+
+// const name2 = "kim";
+// const name2 = "lee"; // Uncaught SyntaxError: Identifier 'name' has already been declared
+
+// console.log(name);
+// var name = "kim";
+
+// //console.log(aaa); aaa is not defined
+
+// //console.log(name2);
+// //ReferenceError: Cannot access 'name2' before initialization
+// let name2 = "lee";
+
+// var globalVariable = "I am global";
+
+// if (globalVariable === "I am global") {
+//   var globalVariable = "am I local?";
+// }
+// console.log(globalVariable); // am I local?
+
+// let globalVariable = "I am global";
+
+// if (globalVariable === "I am global") {
+//   let globalVariable = "am I local?";
+//   let localVariable = "I am local";
+// }
+
+// console.log(globalVariable); // I am global
+// console.log(localVariable);
+// // Uncaught ReferenceError: localVariable is not defined
+let name = "Global Evan";
+
+if (name === "Global Evan") {
+  console.log(name); // Uncaught ReferenceError: Cannot access 'name' before initialization
+  let name = "Local Evan";
+}
