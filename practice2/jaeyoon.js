475206a6c3f1da3259d1e982aaea8288c980370b
DongHyun Kim <developerforus@gmail.com>
2022-05-29 21:19:39 +0900

    jaeyoon2

--- practice2/jaeyoon.js
+++ practice2/jaeyoon.js
@@ -6,10 +6,3 @@ for (i = 0; i < 100; i++) {
 console.timeEnd(" ");
 console.log(a);
 
-b = 0;
-console.time(" ");
-for (i = 0; i < 100; i++) {
-  b = b + i;
-}
-console.timeEnd(" ");
-console.log(a);
